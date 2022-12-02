fdfb16b1ebd90af6acfbd7c03186dbac46a39fb0
pmevers <85088664+pmevers@users.noreply.github.com>
2022-12-02 00:36:35 -0600

    Add paginated API call script

---
+++ paginated-api-calls.ps1
@@ -0,0 +1,23 @@
+# This is an example of a powershell script to call a paginated API to store the output from an API call to a local CSV.
+# This reduces server load by calling the API in pages. It can also help reduce cloud expenses by performing frequent API calls locally and exporting a more structured format to the cloud. 
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Set cache for paginated API call
+$cacheUri = '...InsertLinkHere...&QueryParameters=True&UseCache=True'
+$cache = Invoke-RestMethod -Uri $cacheUri
+
+# Store number of cached pages in a variable
+$totalCalls = [int]$cache.data.(...Path to Number of Pages...) + 1
+
+# Loop through each cached page and append the results to the array
+$array = @()
+for (($i = 1)l $i -lt $totalCalls; $i++)
+{
+    $Uri = '...InsertLinkHere...&QueryParameters=True&Page = $i'
+    $apiData = Invoke-RestMethod -Uri $Uri
+    $array += $apiData.data
+}
+
+# Convert to CSV or other preferred format and output to desired location
+$array | ConvertTo-Csv -NoTypeInformation | Out-File -FilePath "...InsertFilePath..."
